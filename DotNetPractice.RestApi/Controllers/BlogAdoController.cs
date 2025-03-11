451572a13e95c43b92def18e5e6b9904f1c211eb
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2025-03-11 23:35:50 +0630

    [Complete Get, GetById]API_AdoDotNet

---
+++ DotNetPractice.RestApi/Controllers/BlogAdoController.cs
@@ -0,0 +1,86 @@
+﻿using DotNetPractice.RestApi.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DotNetPractice.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogAdoController : ControllerBase
+    {
+        private readonly SqlConnection _connection;
+
+        public BlogAdoController()
+        {
+            _connection = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        }
+
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            string query = "select * from Blog_tbl";
+            _connection.Open();
+            SqlCommand cmd = new SqlCommand(query, _connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            _connection.Close();
+
+            //List<BlogModel> lst = new List<BlogModel>();
+            //foreach (DataRow dr in dt.Rows)
+            //{
+            //    BlogModel blog = new BlogModel()
+            //    {
+            //        BlogId = Convert.ToInt32(dr["BlogId"]),
+            //        BlogTitle = Convert.ToString(dr["BlogTitle"]),
+            //        BlogContent = Convert.ToString(dr["BlogContent"]),
+            //        BlogAuthor = Convert.ToString(dr["BlogAuthor"]),
+            //    };
+            //    lst.Add(blog);
+            //}
+
+            List<BlogModel> lst = dt.AsEnumerable().Select(dr => new BlogModel
+            {
+                BlogId = Convert.ToInt32(dr["BlogId"]),
+                BlogTitle = Convert.ToString(dr["BlogTitle"]),
+                BlogContent = Convert.ToString(dr["BlogContent"]),
+                BlogAuthor = Convert.ToString(dr["BlogAuthor"]),
+            }).ToList();
+
+            return Ok(lst);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetBlog(int id)
+        {
+            string query = "select * from Blog_tbl where BlogId = @BlogId";
+            _connection.Open();
+            SqlCommand cmd = new SqlCommand(query, _connection);
+            cmd.Parameters.AddWithValue("BlogId", id);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            _connection.Close();
+
+            if(dt.Rows.Count == 0)
+            {
+                return NotFound("no data found");
+            }
+
+            DataRow dr = dt.Rows[0];
+
+            var item = new BlogModel
+            {
+                BlogId = Convert.ToInt32(dr["BlogId"]),
+                BlogTitle = Convert.ToString(dr["BlogTitle"]),
+                BlogContent = Convert.ToString(dr["BlogContent"]),
+                BlogAuthor = Convert.ToString(dr["BlogAuthor"]),
+            };
+
+            return Ok(item);
+        }
+    }
+}
