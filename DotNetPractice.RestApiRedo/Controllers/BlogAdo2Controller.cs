2f2fbc2d547294959376158be836302054275d34
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2025-03-19 12:44:30 +0630

    [Complete]AdoService_ApiRedo

---
+++ DotNetPractice.RestApiRedo/Controllers/BlogAdo2Controller.cs
@@ -0,0 +1,147 @@
+﻿using DotNetPractice.RestApiRedo.Model;
+using DotNetPractice.SharedRedo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DotNetPractice.RestApiRedo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogAdo2Controller : ControllerBase
+    {
+        private readonly AdoDotNetService _adoSerivce;
+        private readonly SqlConnection _connection;
+
+        public BlogAdo2Controller()
+        {
+            _adoSerivce = new AdoDotNetService(ConnectionString.sqlConnectionStringBuilder.ConnectionString);
+            _connection = new SqlConnection(ConnectionString.sqlConnectionStringBuilder.ConnectionString);
+        }
+
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            //SqlConnection connection = new SqlConnection(ConnectionString.sqlConnectionStringBuilder.ConnectionString);
+            string query = "Select * from Blog_tbl";
+            List<BlogModel> lst = _adoSerivce.QueryToList<BlogModel>(query);
+
+            return Ok(lst);
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetById(int id)
+        {
+            string query = "Select * from Blog_tbl where BlogId = @BlogId";
+
+            var blog = _adoSerivce.QueryFirstOrDefault<BlogModel>(query, new ParamArray("@BlogId", id));
+            if(blog is null)
+            {
+                return NotFound("no data found");
+            }
+
+            return Ok(blog);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog(BlogModel model)
+        {
+            string query = @"INSERT INTO [dbo].[Blog_tbl]
+           ([BlogTitle]
+           ,[BlogContent]
+           ,[BlogAuthor])
+     VALUES
+           (@BlogTitle
+           ,@BlogContent
+           ,@BlogAuthor)";
+            ParamArray[] paramArray = new ParamArray[]
+            {
+                new ParamArray("BlogTitle", model.BlogTitle),
+                new ParamArray("BlogContent", model.BlogContent),
+                new ParamArray("BlogAuthor", model.BlogAuthor),
+            };
+
+            int result = _adoSerivce.Execute(query, paramArray);
+
+            string message = result > 0 ? "Create Successful" : "Create Failed";
+            return Ok(message);
+        }
+
+        [HttpPut("id")]
+        public IActionResult UpdateBlog(BlogModel model, int id)
+        {
+            string query = @"UPDATE [dbo].[Blog_tbl]
+   SET [BlogTitle] = @BlogTitle
+      ,[BlogContent] = @BlogContent
+      ,[BlogAuthor] = @BlogAuthor
+ WHERE BlogId = @BlogId";
+
+            ParamArray[] param = new ParamArray[]
+            {
+                new ParamArray("BlogId", id),
+                new ParamArray("BlogTitle", model.BlogTitle),
+                new ParamArray("BlogContent", model.BlogContent),
+                new ParamArray("BlogAuthor", model.BlogAuthor)
+            };
+
+            int result = _adoSerivce.Execute(query, param);
+            string message = result > 0 ? "Update successful" : "Update failed";
+            return Ok(message);
+        }
+
+
+        [HttpPatch("id")]
+        public IActionResult PatchBlog(BlogModel model,int id)
+        {
+            string condition = string.Empty;
+            //List<ParamModel> paramModels = new List<ParamModel>();
+            ParamArray[] paramArrays = new ParamArray[4];
+
+            if (!string.IsNullOrEmpty(model.BlogTitle))
+            {
+                condition += "[BlogTitle] = @BlogTitle, ";
+                paramArrays[0] = new ParamArray("BlogTitle", model.BlogTitle);
+            }
+            if (!string.IsNullOrEmpty(model.BlogContent))
+            {
+                condition += "[BlogContent] = @BlogContent, ";
+                paramArrays[1] = new ParamArray("BlogContent", model.BlogContent);
+            }
+            if (!string.IsNullOrEmpty(model.BlogAuthor))
+            {
+                condition += "[BlogAuthor] = @BlogAuthor, ";
+                paramArrays[2] = new ParamArray("BlogAuthor", model.BlogAuthor);
+            }
+
+            paramArrays[3] = new ParamArray("BlogId", id);
+            if (condition.Length == 0)
+            {
+                return BadRequest("No data to update");
+            }
+
+            condition = condition.Substring(0, condition.Length - 2);
+
+
+            string query = $@"UPDATE [dbo].[Blog_tbl]
+   SET {condition} WHERE BlogId = @BlogId";
+
+            int result = _adoSerivce.PatchExecute(query, paramArrays);
+
+            string message = result > 0 ? "Patching successful" : "Patching Failed";
+            return Ok(message);
+
+        }
+
+        [HttpDelete("id")]
+        public IActionResult Delete(int id)
+        {
+            string query = @"DELETE FROM [dbo].[Blog_tbl]
+      WHERE BlogId = @BlogId";
+
+            int result = _adoSerivce.Execute(query, new ParamArray("BlogId", id));
+            string message = result > 0 ? "Delete successful" : "Delete failed";
+            return Ok(message);
+        }
+    }
+}
---
+++ DotNetPractice.SharedRedo/AdoDotNetService.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractice.SharedRedo
+{
+    public class AdoDotNetService
+    {
+        private readonly SqlConnection _connection;
+
+        public AdoDotNetService(string connection)
+        {
+            _connection = new SqlConnection(connection);
+        }
+
+        public List<T> QueryToList<T>(string query, params ParamArray[]? paramArray)
+        {
+            _connection.Open();
+            SqlCommand cmd = new SqlCommand(query, _connection);
+
+            if (paramArray is not null && paramArray.Length > 0)
+            {
+                cmd.Parameters.AddRange(paramArray.Select(x=> new SqlParameter(x.Name, x.Value)).ToArray());
+            }
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            _connection.Close();
+
+            var json = JsonConvert.SerializeObject(dt);
+            List<T> lst = JsonConvert.DeserializeObject<List<T>>(json)!;
+            return lst;
+        }
+
+        public T QueryFirstOrDefault<T>(string query, params ParamArray[]? paramArray)
+        {
+            _connection.Open();
+            SqlCommand cmd = new SqlCommand(query, _connection);
+
+            if (paramArray is not null && paramArray.Length > 0)
+            {
+                cmd.Parameters.AddRange(paramArray.Select(x => new SqlParameter(x.Name, x.Value)).ToArray());
+            }
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            _connection.Close();
+
+            var json = JsonConvert.SerializeObject(dt);
+            List<T> lst = JsonConvert.DeserializeObject<List<T>>(json)!;
+
+            if(lst.Count == 0)
+            {
+                return default;
+            }
+            return lst[0];
+        }
+
+        public int Execute(string query, params ParamArray[] paramArray)
+        {
+            _connection.Open();
+            SqlCommand cmd = new SqlCommand(query, _connection);
+            
+            cmd.Parameters.AddRange(paramArray.Select(x=>new SqlParameter(x.Name, x.Value)).ToArray());
+
+            int result = cmd.ExecuteNonQuery();
+            _connection.Close();
+            return result;
+        }
+
+
+        public int PatchExecute(string query, ParamArray[] paramArray)
+        {
+            _connection.Open();
+            SqlCommand cmd = new SqlCommand(query, _connection);
+
+            foreach (var param in paramArray)
+            {
+                if(param is not null)
+                {
+                    cmd.Parameters.AddWithValue(param.Name, param.Value);
+                }
+            }
+
+            int result = cmd.ExecuteNonQuery();
+            _connection.Close();
+            return result;
+        }
+    }
+
+    public class ParamArray
+    {
+        public ParamArray(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public string Name { get; set; }
+        public object Value { get; set; }
+    }
+}
