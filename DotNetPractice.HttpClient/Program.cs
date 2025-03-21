062e292bd3e98801a3a4756390289af51bd9b578
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2025-03-21 23:29:20 +0630

    [Complete]HttpClient_Console

---
+++ DotNetPractice.HttpClient/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+string json = File.ReadAllText("data.json");
+Console.WriteLine(json);
+
+var model = JsonConvert.DeserializeObject<Dto>(json);
+Console.WriteLine(model);
+
+foreach (var number in model.numberList)
+{
+    Console.WriteLine(number);
+}
+
+
+foreach(var questions in model.questions)
+{
+    Console.WriteLine(questions.questionName);
+}
+
+Console.ReadKey();
+
+public class Dto
+{
+    public Question[] questions { get; set; }
+    public Answer[] answers { get; set; }
+    public string[] numberList { get; set; }
+}
+
+public class Question
+{
+    public int questionNo { get; set; }
+    public string questionName { get; set; }
+}
+
+public class Answer
+{
+    public int questionNo { get; set; }
+    public int answerNo { get; set; }
+    public string answerResult { get; set; }
+}
+
+
+
