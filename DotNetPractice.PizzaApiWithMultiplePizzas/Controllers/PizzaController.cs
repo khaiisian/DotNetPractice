7cab3c9ca11b83d148d55a6c6273932b8a425a7b
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2025-04-12 21:51:10 +0630

    [Complete]MutiplePizzaOrderApi_Response

--- DotNetPractice.PizzaApiWithMultiplePizzas/Controllers/PizzaController.cs
+++ DotNetPractice.PizzaApiWithMultiplePizzas/Controllers/PizzaController.cs
@@ -55,29 +55,6 @@ namespace DotNetPractice.PizzaApiWithMultiplePizzas.Controllers
 
             }
 
-            //var pizza_ids = requestModel.OrderItems.Select(x => x.PizzaId).ToList();
-            //List<int> extra_ids = new List<int>();
-
-            //var pizzas = _appDbContext.Pizzas.Where(x => pizza_ids.Contains(x.PizzaId)).ToList();
-            //var total_amount = pizzas.Sum(x=>x.Price);
-
-            //foreach(var item in requestModel.OrderItems)
-            //{
-            //    if(item.Extras!=null && item.Extras.Length> 0)
-            //    {
-            //        foreach(var extra in item.Extras)
-            //        {
-            //            extra_ids.Add(extra);
-            //        }
-            //    }
-            //}
-
-            //if(extra_ids.Count>0)
-            //{
-            //    var extras = _appDbContext.Extras.Where(x=>extra_ids.Contains(x.ExtraId)).ToList();
-            //    total_amount += extras.Sum(x => x.Price);
-            //}
-
             string Invoice_num = DateTime.Now.ToString("yyyyMMddHHmmss");
             OrderModel order = new OrderModel()
             {
@@ -119,7 +96,43 @@ namespace DotNetPractice.PizzaApiWithMultiplePizzas.Controllers
 
             _appDbContext.SaveChanges();
 
-            return Ok("Order Successful");
+            var resOrder = _appDbContext.Orders.FirstOrDefault(x => x.InvoiceNum == Invoice_num);
+            if (resOrder == null) return NotFound("No order to return");
+            
+            var resOrderItems = _appDbContext.OrderItems.Where(x => x.OrderId == resOrder!.OrderId).ToList();
+
+            var orderItemIdList = _= resOrderItems.Select(x=>x.OrderItemId).ToList();
+            var resOrderExtras = _appDbContext.OrderDetails.Where(x=> orderItemIdList.Contains(x.OrderItemId)).ToList();
+
+            var response_extraLst = resOrderExtras.Select(x =>
+            {
+                var extra = _appDbContext.Extras.FirstOrDefault(a => a.ExtraId == x.ExtraId);
+                return new OrderResExtra
+                {
+                    ExtraName = extra!.ExtraName,
+                    ExtraAmount = $"${extra!.Price}"
+                };
+            }).ToList();
+
+            var response_orderItemlst = resOrderItems.Select(x =>
+            {
+                var pizza = _appDbContext.Pizzas.FirstOrDefault(a => a.PizzaId == x.PizzaId);
+                return new OrderResItem
+                {
+                    PizzaName = pizza!.PizzaName,
+                    PizzaAmount = $"${pizza!.Price}",
+                    OrderResExtra = response_extraLst
+                };
+            }).ToList();
+
+            var respnesOrder = new OrderResponse
+            {
+                InvoiceNum = resOrder.InvoiceNum,
+                OrderResItems = response_orderItemlst,
+                TotalAmount = $"{resOrder.TotalAmount}"
+            };
+
+            return Ok(respnesOrder);
         }
 
 
---
+++ DotNetPractice.PizzaApiWithMultiplePizzas/Model/OrderResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace DotNetPractice.PizzaApiWithMultiplePizzas.Model
+{
+    public class OrderResponse
+    {
+        public string InvoiceNum { get; set; }
+        public List<OrderResItem> OrderResItems { get; set; }
+        public string TotalAmount { get; set; }
+    }
+
+    public class OrderResItem
+    {
+        public string PizzaName { get; set; }
+        public string PizzaAmount { get; set; }
+        public List<OrderResExtra> OrderResExtra { get; set; }
+    }
+
+    public class OrderResExtra
+    {
+        public string ExtraName { get; set; }
+        public string ExtraAmount { get; set; }
+    }
+}
