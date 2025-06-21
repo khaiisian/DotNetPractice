f1948ae3759c29afc7da7e416c8124742db4ef3c
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2025-06-21 23:31:57 +0630

    ApexChart>PieChart [Complete]

---
+++ DotNetPractice.MvcChartApp/Controllers/ApexChartController.cs
@@ -0,0 +1,20 @@
+﻿using DotNetPractice.MvcChartApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetPractice.MvcChartApp.Controllers
+{
+    public class ApexChartController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            List<int> series = new List<int>() {30, 40, 20, 10};
+            List<string> labels = new List<string> { "Team A", "Team B", "Team C", "Team D" };
+            PieChartModel model = new PieChartModel()
+            {
+                series = series,
+                labels = labels,
+            };
+            return View(model);
+        }
+    }
+}
---
+++ DotNetPractice.MvcChartApp/Models/PieChartModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetPractice.MvcChartApp.Models
+{
+    public class PieChartModel
+    {
+        public List<int> series {  get; set; }
+        public List<string> labels { get; set; }
+    }
+}
