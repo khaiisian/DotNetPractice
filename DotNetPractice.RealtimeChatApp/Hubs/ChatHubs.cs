fe988ea95b2840792fe4c1252c8b13efd577f159
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2025-06-29 04:25:38 +0630

    SignalR - RealtimeChat [Complete]

---
+++ DotNetPractice.RealtimeChatApp/Hubs/ChatHubs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace DotNetPractice.RealtimeChatApp.Hubs
+{
+    public class ChatHubs: Hub
+    {
+        public async Task ServerReceiveMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ClientReceiveMessage", user, message);
+        }
+    }
+}
